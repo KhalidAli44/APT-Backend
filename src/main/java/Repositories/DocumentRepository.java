f2599e8ba64215f77fd24cbf6629150d73f944fb
KhalidAli44 <khalid4abdelwahab@gmail.com>
2024-05-06 14:48:12 +0300

    Added DocumentInfo

---
+++ src/main/java/Repositories/DocumentRepository.java
@@ -0,0 +1,9 @@
+package Repositories;
+
+import Model.DocumentInfo;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface DocumentRepository extends MongoRepository<DocumentInfo, String> {
+    // You can define custom query methods here if needed
+}
+
---
+++ src/main/java/Web/DocumentController.java
@@ -0,0 +1,23 @@
+package Web;
+
+import Model.DocumentInfo;
+import Repositories.DocumentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/documents")
+public class DocumentController {
+
+    @Autowired
+    private DocumentRepository documentRepository;
+
+    @PostMapping
+    public ResponseEntity<DocumentInfo> createDocument(@RequestBody DocumentInfo documentInfo) {
+        DocumentInfo createdDocument = documentRepository.save(documentInfo);
+        return new ResponseEntity<>(createdDocument, HttpStatus.CREATED);
+    }
+}
+
