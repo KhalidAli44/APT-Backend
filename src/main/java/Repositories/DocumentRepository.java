17b776e1ff3833110594145ef7236a158c0e5afb
KhalidAli44 <khalid4abdelwahab@gmail.com>
2024-05-06 15:34:41 +0300

    test4

--- src/main/java/Repositories/DocumentRepository.java
+++ src/main/java/Repositories/DocumentRepository.java
@@ -1,9 +1,9 @@
-package Repositories;
-
-import Model.DocumentInfo;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface DocumentRepository extends MongoRepository<DocumentInfo, String> {
-    // You can define custom query methods here if needed
-}
-
+//package Repositories;
+//
+//import Model.DocumentInfo;
+//import org.springframework.data.mongodb.repository.MongoRepository;
+//
+//public interface DocumentRepository extends MongoRepository<DocumentInfo, String> {
+//    // You can define custom query methods here if needed
+//}
+//
