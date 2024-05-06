71572ae7775bb9168969f63f4de3b76111caba5e
KhalidAli44 <khalid4abdelwahab@gmail.com>
2024-05-06 15:53:31 +0300

    test5

--- src/main/java/Model/DocumentInfo.java
+++ src/main/java/Model/DocumentInfo.java
@@ -1,40 +1,40 @@
-//package Model;
-//
-//import org.springframework.data.annotation.Id;
-//import org.springframework.data.mongodb.core.mapping.Document;
-//
-//@Document(collection = "documents")
-//public class DocumentInfo {
-//    @Id
-//    private String id;
-//    private String title;
-//    private String content;
-//
-//    public DocumentInfo() {
-//    }
-//
-//    public String getId() {
-//        return id;
-//    }
-//
-//    public void setId(String id) {
-//        this.id = id;
-//    }
-//
-//    public String getTitle() {
-//        return title;
-//    }
-//
-//    public void setTitle(String title) {
-//        this.title = title;
-//    }
-//
-//    public String getContent() {
-//        return content;
-//    }
-//
-//    public void setContent(String content) {
-//        this.content = content;
-//    }
-//}
-//
+package Model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "documents")
+public class DocumentInfo {
+    @Id
+    private String id;
+    private String title;
+    private String content;
+
+    public DocumentInfo() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
+
