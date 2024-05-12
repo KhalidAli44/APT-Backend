036e6b49bba75577621b315cb0e1d4edaa40cb72
KhalidAli44 <khalid4abdelwahab@gmail.com>
2024-05-13 02:16:56 +0300

    bug fixes

--- src/main/java/com/APT/Backend/Web/WebSocketController.java
+++ src/main/java/com/APT/Backend/Web/WebSocketController.java
@@ -1,16 +1,20 @@
 package com.APT.Backend.Web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class WebSocketController {
 
+    @Autowired
+    private SimpMessagingTemplate messagingTemplate;
+
     @MessageMapping("/operation/{documentId}")
-    @SendTo("/all/broadcast/{documentId}")
-    public String passMessage(@DestinationVariable String documentId, String message) {
-        return message;
+    public void passMessage(@DestinationVariable String documentId, String message) {
+        messagingTemplate.convertAndSend("/all/broadcast/" + documentId, message);
     }
 }
