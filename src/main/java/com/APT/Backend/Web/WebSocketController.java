478d8e0dd1c0b5531d1f715e08fa95c24c1dc7f2
KhalidAli44 <khalid4abdelwahab@gmail.com>
2024-05-14 23:31:27 +0300

    test

--- src/main/java/com/APT/Backend/Web/WebSocketController.java
+++ src/main/java/com/APT/Backend/Web/WebSocketController.java
@@ -2,6 +2,7 @@ package com.APT.Backend.Web;
 
 import com.APT.Backend.Model.MessageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
@@ -22,7 +23,7 @@ public class WebSocketController {
     private int lock = 0;
 
     @MessageMapping("/operation/{documentId}")
-    public void passMessage(final MessageInfo message) {
+    public void passMessage(final MessageInfo message, @DestinationVariable String documentId) {
 //        synchronized(mutex) {
 //            if (lock == 0) {
 //                lock = 1;
@@ -35,6 +36,7 @@ public class WebSocketController {
 //        synchronized(mutex) {
 //            lock = 0;
 //        }
-        messagingTemplate.convertAndSend("/all/broadcast/{documentId}", message);
+        messagingTemplate.convertAndSend("/all/broadcast/" + documentId, message);
+
     }
 }
